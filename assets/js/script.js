c8b5ad579f6248708f3b0280446f46da0cf31c32
wildarte <wildarte10@gmail.com>
2024-05-20 20:35:04 -0300

    comecando a home reponsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    
+    const img_center_card_hori = document.querySelector('.img_center_card_hori img');
+
+    let height_img = img_center_card_hori.getBoundingClientRect().height;
+
+    console.log('height: '+height_img);
+
+    const card_hori = document.querySelectorAll('.card_hori');
+
+    let height_terco = height_img / 3;
+
+    card_hori.forEach((item) => {
+
+        item.style.minHeight = (height_terco - 24)+"px";
+
+    });
     
 });
+
+
+$(document).ready(function(){
+    
+    $('.carousel_testimonials').owlCarousel({
+      loop:true,
+      margin:20,
+      nav:false,
+      dots: true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:3
+          }
+      }
+  })
+
+})
