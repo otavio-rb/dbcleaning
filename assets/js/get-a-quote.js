7df898bcfe411445c71ed7b518fe535a12ffd886
wildarte <wildarte10@gmail.com>
2024-05-23 21:55:10 -0300

    continuacao page get a quote

--- assets/js/get-a-quote.js
+++ assets/js/get-a-quote.js
@@ -48,3 +48,54 @@ function changeValueSquare(delta, event) {
     }
     
 }
+
+
+const over_card_select = document.querySelectorAll('.over_card_select');
+
+over_card_select.forEach((item, index) => {
+    item.addEventListener('click', () => {
+
+        if(!item.classList.contains('active')){
+
+            over_card_select.forEach((item2) => {
+                item2.classList.remove('active');
+                item2.querySelector('.over_checkbox input').checked = false
+            })
+
+            over_card_select[index].classList.add('active');
+        }
+    })
+})
+
+const card_select2 = document.querySelectorAll('.card_select2');
+
+card_select2.forEach((item, index) => {
+    item.addEventListener('click', () => {
+
+        if(!item.classList.contains('active')){
+
+            card_select2.forEach((item2) => {
+                item2.classList.remove('active');
+            })
+
+            card_select2[index].classList.add('active');
+        }
+    })
+})
+
+
+const card_select3 = document.querySelectorAll('.card_select3');
+
+card_select3.forEach((item, index) => {
+    item.addEventListener('click', () => {
+
+        if(!item.classList.contains('active')){
+
+            card_select3.forEach((item2) => {
+                item2.classList.remove('active');
+            })
+
+            card_select3[index].classList.add('active');
+        }
+    })
+})
